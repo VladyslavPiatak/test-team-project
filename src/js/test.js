59807e1f972ebc0ff7c7f47432804da0add83ff6
VladyslavPiatak <vladislawatson@gmail.com>
2023-09-23 13:48:21 +0300

    script test

---
+++ src/js/test.js
@@ -0,0 +1,65 @@
+const textTest = "Hello!";
+let age = 30;
+let undefined 
+
+const textTest1 = `Ім'я ${textTest} Вік: ${age}`;
+
+
+console.log(age > 0);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(textTest);
+console.log(textTest1);
+
+// alert("виведення у вікні монітору вище браузера ");
+// console.log("виведення у вікні монітору вище браузера")
+// const messege = prompt("Ім'я");
+// console.log(messege);
+
+// const messege2 = confirm("Будеш вчити? бульова відповідь на питання");
+// console.log(messege2);
+
+const numb = 5;
+const stri = "0";
+console.log(numb + stri);
+
+let username;
+console.log(typeof username); // "undefined"
+
+let inputValue = null;
+console.log(typeof inputValue); // "object"
+
+const quantity = 17;
+console.log(typeof quantity); // "number"
+
+const message = "JavaScript is awesome!";
+console.log(typeof message); // "string"
+
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen); // "boolean"
+
+
+
+console.log(false == 0); //true
+
+
+console.log("1" != 1); //false
+console.log("1" == 1); //true
+console.log("1" !== 1); //true
+console.log("1" === 1); //false
+
+
+
+
+
+console.log(0 || 3 || true);
+
+console.log("1" + 1); //false
+
+
+
+
+
+
+
+
